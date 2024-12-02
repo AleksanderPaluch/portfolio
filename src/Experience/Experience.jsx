a5d66a58f4f519942ffaf88b4843e3baa6ef47db
Sashiy1 <aleksander.paluch@wp.pl>
2024-12-02 19:39:48 +0100

    expirience reveal added

--- src/Experience/Experience.jsx
+++ src/Experience/Experience.jsx
@@ -1,6 +1,7 @@
 import { div } from "framer-motion/client";
 import SectionHeader from "../SectionHeader/SectionHeader";
 import ExperienceItem from "./ExperienceItem";
+import Reveal from "../Reveal/Reveal";
 
 const expirience = [
   {
@@ -16,34 +17,47 @@ const expirience = [
     time: "02/2022 - 10/2022",
     description: (
       <>
-        <p className="my-2">
-          <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
-            Maximized Profit Margins:
-          </span>
-          Achieved the highest profit margin in the region for four consecutive
-          months
-        </p>
-        <p className="my-2">
-          <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
-            Expanded Client Base:
-          </span>
-          Successfully gained 45 new clients within 7 months, contributing to
-          significant regional growth
-        </p>
-        <p className="my-2">
-          <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
-            Market Analysis
-          </span>
-          Conducted thorough market research and competitor analysis to identify
-          new business opportunities
-        </p>
-        <p className="my-2">
-          <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
-            Customer Relationship Management:
-          </span>
-          I built and maintained strong relationships with existing and new
-          clients
-        </p>
+        {" "}
+        <Reveal>
+          {" "}
+          <p className="my-2">
+            <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
+              Maximized Profit Margins:
+            </span>
+            Achieved the highest profit margin in the region for four
+            consecutive months
+          </p>
+        </Reveal>
+        <Reveal>
+          {" "}
+          <p className="my-2">
+            <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
+              Expanded Client Base:
+            </span>
+            Successfully gained 45 new clients within 7 months, contributing to
+            significant regional growth
+          </p>
+        </Reveal>
+        <Reveal>
+          {" "}
+          <p className="my-2">
+            <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
+              Market Analysis
+            </span>
+            Conducted thorough market research and competitor analysis to
+            identify new business opportunities
+          </p>
+        </Reveal>
+        <Reveal>
+          {" "}
+          <p className="my-2">
+            <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
+              Customer Relationship Management:
+            </span>
+            I built and maintained strong relationships with existing and new
+            clients
+          </p>
+        </Reveal>
       </>
     ),
   },
@@ -54,31 +68,39 @@ const expirience = [
     time: "07/2021 - 02/2022",
     description: (
       <>
-        <p className="my-2">
+        <Reveal>
           {" "}
-          <span className="mr-1 rounded bg-neutral px-2 py-1  font-semibold text-zinc-100">
-            Customer Acquisition:
-          </span>
-          Identified and approached potential clients to expand the customer
-          base
-        </p>
-        <p className="my-2">
+          <p className="my-2">
+            {" "}
+            <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
+              Customer Acquisition:
+            </span>
+            Identified and approached potential clients to expand the customer
+            base
+          </p>
+        </Reveal>
+        <Reveal>
+          <p className="my-2">
+            {" "}
+            <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
+              {" "}
+              Sales Negotiation:
+            </span>
+            Conducted negotiations with clients to secure orders, ensure
+            favorable terms, and close deals
+          </p>
+        </Reveal>
+        <Reveal>
           {" "}
-          <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
-            {" "}
-            Sales Negotiation:
-          </span>
-          Conducted negotiations with clients to secure orders, ensure favorable
-          terms, and close deals
-        </p>
-        <p className="my-2">
-          <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
-            {" "}
-            Customer Support:
-          </span>
-          Provided support to clients, addressing any issues or concerns
-          and ensuring high levels of customer satisfaction
-        </p>
+          <p className="my-2">
+            <span className="mr-1 rounded bg-neutral px-2 py-1 font-semibold text-zinc-100">
+              {" "}
+              Customer Support:
+            </span>
+            Provided support to clients, addressing any issues or concerns and
+            ensuring high levels of customer satisfaction
+          </p>
+        </Reveal>
       </>
     ),
   },
--- src/Experience/ExperienceItem.jsx
+++ src/Experience/ExperienceItem.jsx
@@ -1,15 +1,24 @@
+import Reveal from "../Reveal/Reveal";
+
 const ExperienceItem = ({ company, location, position, time, description }) => {
   return (
-    <div className="">
+    <div>
       <div className="row flex justify-between">
-        <div >
-          <p className="text-3xl font-bold">{company}</p>
-          <p className="text-2xl font-semibold text-primary">{position}</p>
+        <div>
+          <Reveal>
+            <p className="text-3xl font-bold">{company}</p>
+          </Reveal>
+          <Reveal>
+            <p className="text-2xl font-semibold text-primary">{position}</p>
+          </Reveal>
         </div>
-        <div className="my-2">
-          <p className="text-right text-2xl mb-2">{location}</p>
-          <p>{time}</p>
-        </div>
+
+        <Reveal>
+          <div className="my-2">
+            <p className="mb-2 text-right text-2xl">{location}</p>
+            <p>{time}</p>
+          </div>
+        </Reveal>
       </div>
 
       <p>{description}</p>
