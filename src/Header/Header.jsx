c82905002a466787fff5e10a103e534cde43b446
Sashiy1 <aleksander.paluch@wp.pl>
2024-12-03 15:36:51 +0100

    header added

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,12 +3,12 @@ import Links from "../Links/Links";
 
 const Header = () => {
   return (
-    <div className="fixed top-0 z-20 flex w-[97%] items-center justify-between bg-base backdrop-blur px-4 py-1">
+    <div className="fixed top-0 z-20 flex w-[97%] items-center justify-between bg-base backdrop-blur px-4 py-2">
       <div>
         <Links />
       </div>
       <div className="flex items-center gap-2">
-        <button className="btn btn-primary btn-sm  rounded-sm text-xl text-white ">
+        <button onClick={() => window.open("/assets/Oleksandr_Palyukh_JS_DEV.pdf")} className="btn btn-primary btn-sm  rounded-sm text-xl text-white ">
           <a href="">My resume</a>
         </button>
         <ThemeBtn />
