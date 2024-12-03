e757ce3339f2e17b076684bbf9d1eaf0094999ef
Sashiy1 <aleksander.paluch@wp.pl>
2024-12-03 13:03:26 +0100

    card added

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -3,12 +3,12 @@ import { IoMailSharp } from "react-icons/io5";
 const Contact = () => {
   return (
     <section id="contact" className="flex justify-center py-20">
-      <div className="card w-[95%] sm:max-2-[400px] md:min-w-[530px] md:w-4/6 bg-neutral shadow-xl lg:w-3/5">
+      <div className="card w-[95%] sm:max-2-[400px] md:min-w-[530px] md:w-4/6 bg-neutral shadow-2xl lg:w-3/5">
         <div className="card-body grid gap-8">
-          <h2 className="text-center text-5xl font-bold md:text-6xl">
+          <h2 className="text-zinc-200 text-center text-5xl font-bold md:text-6xl">
             Contact<span className="text-primary">.</span>
           </h2>
-          <p className="text-sm  text-center md:text-xl">
+          <p className="text-zinc-200 text-sm  text-center md:text-xl">
             Shoot me an email if you want to connect! You can also find me on{" "}
             <span className="text-primary hover:underline">
               <a
@@ -21,9 +21,9 @@ const Contact = () => {
             </span>{" "}
             if that&apos;s more your speed.
           </p>
-          <a className="text-md md:text-3xl font-semibold flex items-center gap-2 justify-center transition-colors hover:text-primary" href="mailto:aleksanderpaluch52@gmail.com">
+          <a className="text-zinc-200 text-md md:text-xl font-semibold flex items-center gap-2 justify-center transition-colors hover:text-primary" href="mailto:aleksanderpaluch52@gmail.com">
             {" "}
-            <span className="md:pt-1.5">
+            <span className="md:pt-0.5">
               <IoMailSharp />
             </span>{" "}
             aleksanderpaluch52@gmail.com
