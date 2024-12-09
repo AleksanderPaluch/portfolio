518dec62a25e19f9bbd5ff3885bafe4b39381269
Sashiy1 <aleksander.paluch@wp.pl>
2024-12-09 14:25:48 +0100

    padding fix

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,22 +3,44 @@ import Links from "../Links/Links";
 
 const Header = ({ setIsPolish, isPolish }) => {
   return (
-    <div className="fixed top-0 z-20 flex w-[90%] md:w-[94%] lg:w-[97%] items-center justify-between bg-base backdrop-blur px-4 py-2">
+    <div className="bg-base fixed top-0 z-20 flex w-[90%] items-center justify-between px-4 py-2 backdrop-blur md:w-[94%] lg:w-[97%]">
       <div>
         <Links />
       </div>
       <div className="flex items-center gap-2">
         {/* Language Toggle Button */}
-        <button onClick={() => setIsPolish(!isPolish)} className="btn-sm btn-ghost rounded-sm ">
-          {isPolish 
-            ? <img className="w-6" src="../../assets/Flag_of_the_United_Kingdom_(3-5).svg" alt="English Flag" />
-            : <img className="w-6 border-[1px] border-zinc-700" src="../../assets/Flag_of_Poland.svg" alt="Polish Flag" />
-          }
+        <button
+          onClick={() => setIsPolish(!isPolish)}
+          className="btn-ghost btn-sm rounded-sm"
+        >
+          {isPolish ? (
+            <img
+              className="w-6"
+              src="../../assets/Flag_of_the_United_Kingdom_(3-5).svg"
+              alt="English Flag"
+            />
+          ) : (
+            <img
+              className="w-6 border-[1px] border-zinc-700"
+              src="../../assets/Flag_of_Poland.svg"
+              alt="Polish Flag"
+            />
+          )}
         </button>
 
         {/* Resume Button */}
-        <button onClick={() => window.open("../../assets/Oleksandr_Palyukh_FullStack_ENG.cv.pdf")} className="btn btn-primary min-w-[150px] btn-sm  rounded-sm  text-xl text-white ">
-          <a href="">{isPolish ? "Moje CV" : "My Resume"}</a>
+        <button
+          onClick={() =>
+            window.open(
+              isPolish
+                ? "../../assets/Oleksandr_Palyukh_FullStack.cv.PL.pdf"
+                : "../../assets/Oleksandr_Palyukh_FullStack_ENG.cv.pdf",
+              "_blank"
+            )
+          }
+          className="btn btn-primary btn-sm min-w-[150px] rounded-sm text-xl text-white"
+        >
+          {isPolish ? "Moje CV" : "My Resume"}
         </button>
 
         {/* Theme Button */}
